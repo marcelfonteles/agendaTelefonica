9764db778de802e626c4fcbc341ddb5967b59fea
Marcel <marcelfonteles@gmail.com>
2019-05-09 01:44:04 +0000

    adding contacts hash and one checkbox

--- controllers/agenda_controller.js
+++ controllers/agenda_controller.js
@@ -2,4 +2,11 @@ var app = angular.module("agenda");
 
 app.controller("agendaCtrl", ["$scope", function($scope) {
   $scope.app = "Agenda Telefonica";
+  $scope.contacts = [
+      {nome: "Marcel Vieira", telefone: "+55 85 9 9999-8888", operadora: 41},
+      {nome: "José Marques", telefone: "+55 85 9 9999-8888", operadora: 31},
+      {nome: "João Gilberto", telefone: "+55 85 9 9999-8888", operadora: 25},
+      {nome: "Guimarães Rosa", telefone: "+55 85 9 9999-8888", operadora: 15},
+      {nome: "Jimmy Neutron", telefone: "+55 85 9 9999-8888", operadora: 21}
+    ];
 }])
