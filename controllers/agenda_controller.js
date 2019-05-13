a6c3864ddb8d73e21f1aa9d608216d9a3baa01a0
Marcel <marcelfonteles@gmail.com>
2019-05-13 23:53:32 +0000

    adicionando operadoras e categorizando por tipo

--- controllers/agenda_controller.js
+++ controllers/agenda_controller.js
@@ -3,13 +3,21 @@ var app = angular.module("agenda");
 app.controller("agendaCtrl", ["$scope", function($scope) {
   $scope.app = "Agenda Telefonica";
   $scope.contacts = [
-      {nome: "Marcel Vieira", telefone: "+55 85 9 9999-8888", operadora: 41},
-      {nome: "José Marques", telefone: "+55 85 9 9999-8888", operadora: 31},
-      {nome: "João Gilberto", telefone: "+55 85 9 9999-8888", operadora: 25},
-      {nome: "Guimarães Rosa", telefone: "+55 85 9 9999-8888", operadora: 15},
-      {nome: "Jimmy Neutron", telefone: "+55 85 9 9999-8888", operadora: 21}
+      {nome: "Marcel Vieira", telefone: "+55 85 9 9999-8888", codigo: 41},
+      {nome: "José Marques", telefone: "+55 85 9 9999-8888", codigo: 31},
+      {nome: "João Gilberto", telefone: "+55 85 9 9999-8888", codigo: 25},
+      {nome: "Guimarães Rosa", telefone: "+55 85 9 9999-8888", codigo: 15},
+      {nome: "Jimmy Neutron", telefone: "+55 85 9 9999-8888", codigo: 21}
+    ];
+  $scope.operadoras = [
+      {nome: "Tim", codigo: 41, categoria: "Celular"},
+      {nome: "Oi", codigo: 31, categoria: "Celular"},
+      {nome: "GVT", codigo: 25, categoria: "Fixo"},
+      {nome: "Vivo", codigo: 15, categoria: "Celular"},
+      {nome: "Multiplay", codigo: 21, categoria: "Fixo"}
     ];
   $scope.addContact = function(contact) {
+    contact.codigo = contact.codigo.codigo;
     $scope.contacts.push(angular.copy(contact));
     delete $scope.contact;
   };
@@ -23,4 +31,10 @@ app.controller("agendaCtrl", ["$scope", function($scope) {
       return !contact.select;
     });
   }; 
+  $scope.findOperadora = function(codigo) {
+    var operadora = $scope.operadoras.filter(function(operadora) {
+        return operadora.codigo === codigo;
+    });
+    return operadora[0].nome;
+  };
 }])
